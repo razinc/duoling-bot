bb8c1cc808586ae6a4a4c57f22da16bf267d6583
razinc <l_noir@yahoo.com>
2022-10-03 19:11:44 +0800

    Create syllabus.py

---
+++ syllabus.py
@@ -0,0 +1,9 @@
+syllabus = {
+    "Unit 2": {
+        "Hobbies 2": {
+            "english": "His hobby is listening to music",
+            "chinese": "他的爱好是听音乐",
+            "pinyin": "Tā de àihào shì tīng yīnyuè"
+        }
+    }
+}
