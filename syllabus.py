f541b36bb6a1e4c4c99a57960dea93abac65a9d0
razinc <l_noir@yahoo.com>
2022-10-05 19:45:07 +0800

    Update syllabus.py

--- syllabus.py
+++ syllabus.py
@@ -3,15 +3,19 @@ syllabus = {
         "Hobbies 2": [{
             "english": "His hobby is listening to music",
             "chinese": "他的爱好是听音乐",
-            "pinyin": "Tā de àihào shì tīng yīnyuè"
+            "pinyin" : "Tā de àihào shì tīng yīnyuè"
         }, {
             "english": "I can't speak Spanish",
             "chinese": "我不会说西班牙语",
-            "pinyin": "Wǒ bù huì shuō xībānyá yǔ"
+            "pinyin" : "Wǒ bù huì shuō xībānyá yǔ"
         }, {
             "english": "I am too busy, I just want to relax a bit",
             "chinese": "我太忙了，我只想轻松一下",
-            "pinyin": "Wǒ tài mángle, wǒ zhǐ xiǎng qīngsōng yīxià"
+            "pinyin" : "Wǒ tài mángle, wǒ zhǐ xiǎng qīngsōng yīxià"
+        }, {
+            "english": "I want to travel and relax a bit",
+            "chinese": "我想去旅游轻松一下",
+            "pinyin" : "Wǒ xiǎng qù lǚyóu qīngsōng yīxià"
         }]
     }
 }
