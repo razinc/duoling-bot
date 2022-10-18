49e81c3de0b64a98a50acb71119931477c796371
razinc <l_noir@yahoo.com>
2022-10-18 19:52:32 +0800

    Update syllabus.py

--- syllabus.py
+++ syllabus.py
@@ -1,53 +1,57 @@
 syllabus = {
     "Unit 2": {
         "Hobbies 2": [{
-            "english": "His hobby is listening to music",
-            "chinese": "他的爱好是听音乐",
-            "pinyin" : "Tā de àihào shì tīng yīnyuè"
+            "english": "His hobby is listening to music.",
+            "chinese": "他的爱好是听音乐.",
+            "pinyin" : "Tā de àihào shì tīng yīnyuè."
         }, {
-            "english": "I can't speak Spanish",
-            "chinese": "我不会说西班牙语",
-            "pinyin" : "Wǒ bù huì shuō xībānyá yǔ"
+            "english": "I can't speak Spanish.",
+            "chinese": "我不会说西班牙语.",
+            "pinyin" : "Wǒ bù huì shuō xībānyá yǔ."
         }, {
-            "english": "I am too busy, I just want to relax a bit",
-            "chinese": "我太忙了，我只想轻松一下",
-            "pinyin" : "Wǒ tài mángle, wǒ zhǐ xiǎng qīngsōng yīxià"
+            "english": "I am too busy, I just want to relax a bit.",
+            "chinese": "我太忙了，我只想轻松一下.",
+            "pinyin" : "Wǒ tài mángle, wǒ zhǐ xiǎng qīngsōng yīxià."
         }, {
-            "english": "I want to travel and relax a bit",
-            "chinese": "我想去旅游轻松一下",
-            "pinyin" : "Wǒ xiǎng qù lǚyóu qīngsōng yīxià"
+            "english": "I want to travel and relax a bit.",
+            "chinese": "我想去旅游轻松一下.",
+            "pinyin" : "Wǒ xiǎng qù lǚyóu qīngsōng yīxià."
         }, {
-            "english": "This is too expensive so I'm not buying it",
-            "chinese": "这个太贵，所以我不买",
-            "pinyin" : "Zhège tài guì, suǒyǐ wǒ bú mǎi"
+            "english": "This is too expensive so I'm not buying it.",
+            "chinese": "这个太贵，所以我不买.",
+            "pinyin" : "Zhège tài guì, suǒyǐ wǒ bú mǎi."
         }, {
-            "english": "We will travel to India in March next year",
-            "chinese": "明年三月我们会去印度旅游",
-            "pinyin" : "Míngnián sān yuè wǒmen huì qù yìndù lǚyóu"
+            "english": "We will travel to India in March next year.",
+            "chinese": "明年三月我们会去印度旅游.",
+            "pinyin" : "Míngnián sān yuè wǒmen huì qù yìndù lǚyóu."
         }, {
-            "english": "I like taking pictures with the phone the most",
-            "chinese": "我最喜欢用手机拍照",
-            "pinyin" : "Wǒ zuì xǐhuān yòng shǒujī pāizhào"
+            "english": "I like taking pictures with the phone the most.",
+            "chinese": "我最喜欢用手机拍照.",
+            "pinyin" : "Wǒ zuì xǐhuān yòng shǒujī pāizhào."
         }, {
-            "english": "This is our Spanish teacher",
-            "chinese": "这是我们的西班牙语老师",
-            "pinyin" : "Zhè shì wǒmen de xībānyá yǔ lǎoshī"
+            "english": "This is our Spanish teacher.",
+            "chinese": "这是我们的西班牙语老师.",
+            "pinyin" : "Zhè shì wǒmen de xībānyá yǔ lǎoshī."
         }, {
             "english": "What are your hobbies?",
             "chinese": "你的爱好是什么?",
             "pinyin" : "Nǐ de àihào shì shénme?"
         }, {
-            "english": "She is an Indian",
-            "chinese": "她是印度人",
-            "pinyin" : "Tā shì yìn duó rén"
+            "english": "She is an Indian.",
+            "chinese": "她是印度人.",
+            "pinyin" : "Tā shì yìn duó rén."
         }, {
-            "english": "My hobby is photography",
-            "chinese": "我的爱好是拍照",
-            "pinyin" : "Wǒ de àihào shì pāizhào"
+            "english": "My hobby is photography.",
+            "chinese": "我的爱好是拍照.",
+            "pinyin" : "Wǒ de àihào shì pāizhào."
         }, {
-            "english": "I am learning Chinese because I need to go to China",
-            "chinese": "我学习中文，因为我要去中国",
-            "pinyin" : "Wǒ xuéxí zhōngwén, yīnwèi wǒ yào qù zhōngguó"
+            "english": "I am learning Chinese because I need to go to China.",
+            "chinese": "我学习中文，因为我要去中国.",
+            "pinyin" : "Wǒ xuéxí zhōngwén, yīnwèi wǒ yào qù zhōngguó."
+        }, {
+            "english": "He wants to live in Spain because his girlfriend is Spanish.",
+            "chinese": "他想住在西班牙，因为他的女朋友是西班牙人.",
+            "pinyin" : "Tā xiǎng zhù zài xībānyá, yīnwèi tā de nǚ péngyǒu shì xībānyá rén."
         }]
     }
 }
