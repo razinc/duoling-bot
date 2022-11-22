7fb24426e29515309e84d6a81dd8af06a08cac9c
razinc <l_noir@yahoo.com>
2022-11-22 18:33:29 +0800

    Update syllabus.py

--- syllabus.py
+++ syllabus.py
@@ -1,122 +1,99 @@
 syllabus = {
-    "unit 1":{"description": "Say hello and goodbye, use numbers.",
-              "phrases": [
-                  {"english": "Hello.",
-                   "chinese": "你好.",
-                   "pinyin" : "Nǐ hǎo."},
-                  {"english": "Goodbye.",
-                   "chinese": "再见.",
-                   "pinyin" : "Zàijiàn."},
-                  {"english": "One, two, three, four, five, six, seven, eight, nine, ten.",
-                   "chinese": "一, 二, 三, 四, 五, 六, 七, 八, 九, 十.",
-                   "pinyin" : "Yī, èr, sān, sì, wǔ, liù, qī, bā, jiǔ, shí."},
-                  {"english": "Twelve.",
-                   "chinese": "十二.",
-                   "pinyin" : "Shíèr."},
-                  {"english": "Sixty seven.",
-                   "chinese": "六十七.",
-                   "pinyin" : "Liùshíqī."},
-                  {"english": "One hundred.",
-                   "chinese": "一百.",
-                   "pinyin" : "Yībǎi."}]
-             },
-    "unit 2":{"description": "Ask someone's name, greet people.",
-              "phrases": [
-                  {"english": "Hello, what's your name?",
-                   "chinese": "你好你叫什么名字?",
-                   "pinyin" : "Nǐ hǎo nǐ jiào shénme míngzì?"},
-                  {"english": "My name is Wang Ming, what's yours?",
-                   "chinese": "我叫王明, 你呢?",
-                   "pinyin" : "Wǒ jiào wáng míng, nǐ ne?"},
-                  {"english": "My name is Li Ming.",
-                   "chinese": "我叫李明.",
-                   "pinyin" : "Wǒ jiào lǐ míng."},
-                  {"english": "My last name is Li, what's your last name?",
-                   "chinese": "我姓李, 你姓什么?",
-                   "pinyin" : "Wǒ xìng lǐ, nǐ xìng shénme?"},
-                  {"english": "Nice to meet you.",
-                   "chinese": "很高兴认识你.",
-                   "pinyin" : "Hěn gāoxìng rènshí nǐ."},
-                  {"english": "Nice to meet you too.",
-                   "chinese": "我也很高兴认识你.",
-                   "pinyin" : "Wǒ yě hěn gāoxìng rènshí nǐ."},
-                  {"english": "I am happy.",
-                   "chinese": "我很高兴.",
-                   "pinyin" : "Wǒ hěn gāoxìng."},
-                  {"english": "I know Zhang Ming.",
-                   "chinese": "我认识张明.",
-                   "pinyin" : "Wǒ rènshí zhāng míng."}]
-             },
-    "unit 3":{"description": "Discuss food choices, talk about occupations.",
-              "phrases": [
-                  {"english": "You eat noodles.",
-                   "chinese": "你吃面.",
-                   "pinyin" : "Nǐ chī miàn."},
-                  {"english": "I drink tea.",
-                   "chinese": "我喝茶.",
-                   "pinyin" : "Wǒ hē chá."},
-                  {"english": "I don't eat noodle and also don't eat rice.",
-                   "chinese": "我不吃面也不吃饭.",
-                   "pinyin" : "Wǒ bù chī miàn yě bù chī fàn."}]
-             },
-    "unit 4":{"description": "",
-              "phrases": []
-             },
-    "unit 5":{"description": "",
-              "phrases": []
-             },
-    "unit 6":{"description": "",
-              "phrases": []
-             },
-    "unit 7":{"description": "",
-              "phrases": []
-             },
-    "unit 8":{"description": "",
-              "phrases": []
-             },
-    "unit 9":{"description": "",
-              "phrases": []
-             },
-    "unit 10":{"description": "",
-              "phrases": []
-             },
-    "unit 11":{"description": "",
-              "phrases": []
-             },
-    "unit 12":{"description": "",
-              "phrases": []
-             },
-    "unit 13":{"description": "",
-              "phrases": []
-             },
-    "unit 14":{"description": "",
-              "phrases": []
-             },
-    "unit 15":{"description": "",
-              "phrases": []
-             },
-    "unit 16":{"description": "",
-              "phrases": []
-             },
-    "unit 17":{"description": "",
-              "phrases": []
-             },
-    "unit 18":{"description": "",
-              "phrases": []
-             },
-    "unit 19":{"description": "",
-              "phrases": []
-             },
-    "unit 20":{"description": "",
-              "phrases": []
-             },
-    "unit 21":{"description": "",
-              "phrases": []
-             },
-    "unit 22":{"description": "",
-              "phrases": []
-             },
-    "unit 23":{"description": "",
-              "phrases": []
-             },
+    "unit 1": {
+        "description": "Say hello and goodbye, use numbers.",
+        "phrases": [
+            {"english": "Hello.", "chinese": "你好.", "pinyin": "Nǐ hǎo."},
+            {"english": "Goodbye.", "chinese": "再见.", "pinyin": "Zàijiàn."},
+            {
+                "english": "One, two, three, four, five, six, seven, eight, nine, ten.",
+                "chinese": "一, 二, 三, 四, 五, 六, 七, 八, 九, 十.",
+                "pinyin": "Yī, èr, sān, sì, wǔ, liù, qī, bā, jiǔ, shí.",
+            },
+            {"english": "Twelve.", "chinese": "十二.", "pinyin": "Shíèr."},
+            {"english": "Sixty seven.", "chinese": "六十七.", "pinyin": "Liùshíqī."},
+            {"english": "One hundred.", "chinese": "一百.", "pinyin": "Yībǎi."},
+        ],
+    },
+    "unit 2": {
+        "description": "Ask someone's name, greet people.",
+        "phrases": [
+            {
+                "english": "Hello, what's your name?",
+                "chinese": "你好你叫什么名字?",
+                "pinyin": "Nǐ hǎo nǐ jiào shénme míngzì?",
+            },
+            {
+                "english": "My name is Wang Ming, what's yours?",
+                "chinese": "我叫王明, 你呢?",
+                "pinyin": "Wǒ jiào wáng míng, nǐ ne?",
+            },
+            {
+                "english": "My name is Li Ming.",
+                "chinese": "我叫李明.",
+                "pinyin": "Wǒ jiào lǐ míng.",
+            },
+            {
+                "english": "My last name is Li, what's your last name?",
+                "chinese": "我姓李, 你姓什么?",
+                "pinyin": "Wǒ xìng lǐ, nǐ xìng shénme?",
+            },
+            {
+                "english": "Nice to meet you.",
+                "chinese": "很高兴认识你.",
+                "pinyin": "Hěn gāoxìng rènshí nǐ.",
+            },
+            {
+                "english": "Nice to meet you too.",
+                "chinese": "我也很高兴认识你.",
+                "pinyin": "Wǒ yě hěn gāoxìng rènshí nǐ.",
+            },
+            {"english": "I am happy.", "chinese": "我很高兴.", "pinyin": "Wǒ hěn gāoxìng."},
+            {
+                "english": "I know Zhang Ming.",
+                "chinese": "我认识张明.",
+                "pinyin": "Wǒ rènshí zhāng míng.",
+            },
+        ],
+    },
+    "unit 3": {
+        "description": "Discuss food choices, talk about occupations.",
+        "phrases": [
+            {
+                "english": "You eat noodles.",
+                "chinese": "你吃面.",
+                "pinyin": "Nǐ chī miàn.",
+            },
+            {"english": "I drink tea.", "chinese": "我喝茶.", "pinyin": "Wǒ hē chá."},
+            {
+                "english": "I don't eat noodle and also don't eat rice.",
+                "chinese": "我不吃面也不吃饭.",
+                "pinyin": "Wǒ bù chī miàn yě bù chī fàn.",
+            },
+            {
+                "english": "You drink water.",
+                "chinese": "你喝水.",
+                "pinyin": "Nǐ hē shuǐ.",
+            }
+        ],
+    },
+    "unit 4": {"description": "", "phrases": []},
+    "unit 5": {"description": "", "phrases": []},
+    "unit 6": {"description": "", "phrases": []},
+    "unit 7": {"description": "", "phrases": []},
+    "unit 8": {"description": "", "phrases": []},
+    "unit 9": {"description": "", "phrases": []},
+    "unit 10": {"description": "", "phrases": []},
+    "unit 11": {"description": "", "phrases": []},
+    "unit 12": {"description": "", "phrases": []},
+    "unit 13": {"description": "", "phrases": []},
+    "unit 14": {"description": "", "phrases": []},
+    "unit 15": {"description": "", "phrases": []},
+    "unit 16": {"description": "", "phrases": []},
+    "unit 17": {"description": "", "phrases": []},
+    "unit 18": {"description": "", "phrases": []},
+    "unit 19": {"description": "", "phrases": []},
+    "unit 20": {"description": "", "phrases": []},
+    "unit 21": {"description": "", "phrases": []},
+    "unit 22": {"description": "", "phrases": []},
+    "unit 23": {"description": "", "phrases": []},
 }
