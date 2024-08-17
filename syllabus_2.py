c800faa116fccf6d3bd58bd2fc7b5056fd5fc95c
razinc <l_noir@yahoo.com>
2024-08-17 08:41:29 +0800

    Create syllabus_2.py

---
+++ syllabus_2.py
@@ -0,0 +1,480 @@
+syllabus = {
+    "unit 57": {
+        "description": "Chat with colleagues.",
+        "guide_book": "https://www.duolingo.com/guidebook/zh/57",
+        "phrases": [
+            {
+                "english": "I will have dinner with colleagues later.",
+                "pinyin": "我一会儿会跟同事吃晚饭.",
+                "pinyin": "Wǒ yīhuǐ'er huì gēn tóngshì chī wǎnfàn.",
+            },
+            {
+                "english": "Her relationships with her colleagues and manager are all good.",
+                "pinyin": "她跟她的同事和经理的关系都很好.",
+                "pinyin": "Tā gēn tā de tóngshì hé jīnglǐ de guānxì dōu hěn hǎo.",
+            },
+            {
+                "english": "Can this problem be solved?",
+                "pinyin": "这个问题能解决吗?",
+                "pinyin": "Zhège wèntí néng jiějué ma?",
+            },
+            {
+                "english": "Don't forget!",
+                "pinyin": "别忘了!",
+                "pinyin": "Bié wàngle!",
+            },
+            {
+                "english": "I have a solution.",
+                "pinyin": "我有一个办法.",
+                "pinyin": "Wǒ yǒu yīgè bànfǎ.",
+            },
+            {
+                "english": "What solutions do you have?",
+                "pinyin": "你有什么办法?",
+                "pinyin": "Nǐ yǒu shé me bànfǎ?",
+            },
+            {
+                "english": "You have to believe in yourself.",
+                "pinyin": "你要相信你自己.",
+                "pinyin": "Nǐ yào xiāngxìn nǐ zìjǐ.",
+            },
+            {
+                "english": "It is a pleasure to meet you, Mr. Wang!",
+                "pinyin": "王先生, 认识你很高兴!",
+                "pinyin": "Wáng xiānshēng, rènshí nǐ hěn gāoxìng!",
+            },
+            {
+                "english": "Please believe me, I can solve it.",
+                "pinyin": "请相信我, 我可以解决.",
+                "pinyin": "Qǐng xiāngxìn wǒ, wǒ kěyǐ jiějué.",
+            },
+            {
+                "english": "Do you believe her?",
+                "pinyin": "你相信她吗?",
+                "pinyin": "Nǐ xiāngxìn tā ma?",
+            },
+            {
+                "english": "He forgot there was an important meeting today.",
+                "pinyin": "他忘了今天有重要的会议.",
+                "pinyin": "Tā wàngle jīntiān yǒu zhòngyào de huìyì.",
+            },
+            {
+                "english": "It's suddenly raining!",
+                "pinyin": "突然下雨了!",
+                "pinyin": "Túrán xià yǔle!",
+            },
+            {
+                "english": "Is the new manager coming next month?",
+                "pinyin": "新经理下个月来吗?",
+                "pinyin": "Xīn jīnglǐ xià gè yuè lái ma?",
+            },
+            {
+                "english": "I forgot his birthday!",
+                "pinyin": "我忘了他的生日!",
+                "pinyin": "Wǒ wàngle tā de shēngrì!",
+            },
+            {
+                "english": "Mr. Wang is our new colleague.",
+                "pinyin": "王先生是我们的新同事.",
+                "pinyin": "Wáng xiānshēng shì wǒmen de xīn tóngshì.",
+            },
+            {
+                "english": "Can you help us solve this?",
+                "pinyin": "你能帮我们解决吗?",
+                "pinyin": "Nǐ néng bāng wǒmen jiějué ma?",
+            },
+            {
+                "english": "Unexpectedly, he has an important meeting today, so he cannot come.",
+                "pinyin": "他今天突然有重要的会议, 所以不能来.",
+                "pinyin": "Tā jīntiān túrán yǒu zhòngyào de huìyì, suǒyǐ bùnéng lái.",
+            },
+            {
+                "english": "We have a conference tomorrow.",
+                "pinyin": "我们明天有一个会议.",
+                "pinyin": "",
+            },
+            {
+                "english": "Miss Li is our manager.",
+                "pinyin": "李小姐是我们的经理.",
+                "pinyin": "Wǒmen míngtiān yǒu yīgè huìyì.",
+            },
+            {
+                "english": "She suddenly left, we don't know why.",
+                "pinyin": "她突然离开了, 我们不知道为什么.",
+                "pinyin": "Tā túrán líkāile, wǒmen bù zhīdào wèishéme.",
+            },
+            {
+                "english": "We don't have a solution, sorry.",
+                "pinyin": "我们没有办法, 对不起.",
+                "pinyin": "Wǒmen méiyǒu bànfǎ, duìbùqǐ.",
+            },
+            {
+                "english": "This is Miss Li.",
+                "pinyin": "这是李小姐.",
+                "pinyin": "Zhè shì lǐ xiǎojiě.",
+            },
+            {
+                "english": "Is this important?",
+                "pinyin": "这个重要吗?",
+                "pinyin": "Zhège zhòngyào ma?",
+            },
+            {
+                "english": "Mr. Li, this is Miss Wang.",
+                "pinyin": "李先生, 这是王小姐.",
+                "pinyin": "Lǐ xiānshēng, zhè shì wáng xiǎojiě.",
+            },
+        ],
+    },
+    "unit 58": {
+        "description": "Talk about hobbies.",
+        "guide_book": "https://www.duolingo.com/guidebook/zh/58",
+        "phrases": [
+            {
+                "english": "I don't really like hiking.",
+                "pinyin": "我不太喜欢爬山.",
+                "pinyin": "Wǒ bù tài xǐhuān páshān.",
+            },
+            {
+                "english": "I love watching kung fu movies.",
+                "pinyin": "我爱看功夫电影.",
+                "pinyin": "Wǒ ài kàn gōngfū diànyǐng.",
+            },
+            {
+                "english": "His dad often reads history books.",
+                "pinyin": "他的爸爸经常读历史书.",
+                "pinyin": "Tā de bàba jīngcháng dú lìshǐ shū.",
+            },
+            {
+                "english": "They play mahjong every day.",
+                "pinyin": "他们每天都打麻将.",
+                "pinyin": "Tāmen měitiān dū dǎ májiàng.",
+            },
+            {
+                "english": "Are you interested in British history and culture?",
+                "pinyin": "你对英国的历史和文化有兴趣吗?",
+                "pinyin": "Nǐ duì yīngguó de lìshǐ hé wénhuà yǒu xìngqù ma?",
+            },
+            {
+                "english": "I am not interested.",
+                "pinyin": "我没有兴趣.",
+                "pinyin": "Wǒ méiyǒu xìngqù.",
+            },
+            {
+                "english": "I think Chinese characters look special.",
+                "pinyin": "我觉得汉字看起来很特别.",
+                "pinyin": "Wǒ juédé hànzì kàn qǐlái hěn tèbié.",
+            },
+            {
+                "english": "I have no interest in him.",
+                "pinyin": "我对他没有兴趣.",
+                "pinyin": "Wǒ duì tā méiyǒu xìngqù.",
+            },
+            {
+                "english": "I would like to start learning kung fu.",
+                "pinyin": "我想开始学功夫.",
+                "pinyin": "Wǒ xiǎng kāishǐ xué gōngfū.",
+            },
+            {
+                "english": "I go to China in order to improve my Chinese.",
+                "pinyin": "我去中国是为了提高我的中文水平.",
+                "pinyin": "Wǒ qù zhōngguó shì wèile tígāo wǒ de zhōngwén shuǐpíng.",
+            },
+            {
+                "english": "Chinese tea culture is famous.",
+                "pinyin": "中国的茶文化很有名.",
+                "pinyin": "Zhōngguó de chá wénhuà hěn yǒumíng.",
+            },
+            {
+                "english": "I often go cycling alone on weekends.",
+                "pinyin": "周末我经常一个人骑自行车.",
+                "pinyin": "Zhōumò wǒ jīngcháng yīgè rén qí zìxíngchē.",
+            },
+            {
+                "english": "I want to learn how to play mahjong.",
+                "pinyin": "我想学打麻将.",
+                "pinyin": "Wǒ xiǎng xué dǎ májiàng.",
+            },
+            {
+                "english": "She is interested in American culture.",
+                "pinyin": "她对美国文化感兴趣.",
+                "pinyin": "Tā duì měiguó wénhuà gǎn xìngqù.",
+            },
+            {
+                "english": "The weather is good, let's go hiking!",
+                "pinyin": "天气很好, 我们去爬山吧!",
+                "pinyin": "Tiānqì hěn hǎo, wǒmen qù páshān ba!",
+            },
+            {
+                "english": "We will go to France or Italy.",
+                "pinyin": "我们会去法国或者意大利.",
+                "pinyin": "Wǒmen huì qù fàguó huòzhě yìdàlì.",
+            },
+            {
+                "english": "We are all interested in languages.",
+                "pinyin": "我们都对语言感兴趣.",
+                "pinyin": "Wǒmen dōu duì yǔyán gǎn xìngqù.",
+            },
+            {
+                "english": "Can you teach me to play mahjong?",
+                "pinyin": "你能教我打麻将吗?",
+                "pinyin": "Nǐ néng jiào wǒ dǎ májiàng ma.",
+            },
+            {
+                "english": "In order to have a good job, she studies hard.",
+                "pinyin": "为了有一个好工作, 她努力学习.",
+                "pinyin": "Wèile yǒu yīgè hǎo gōngzuò, tā nǔlì xuéxí.",
+            },
+            {
+                "english": "I am not interested in history.",
+                "pinyin": "我对历史不感兴趣.",
+                "pinyin": "Wǒ duì lìshǐ bùgǎn xìngqù.",
+            },
+            {
+                "english": "He likes exercising, especially swimming.",
+                "pinyin": "他很喜欢做运动, 特别是游泳.",
+                "pinyin": "Tā hěn xǐhuān zuò yùndòng, tèbié shì yóuyǒng.",
+            },
+            {
+                "english": "Would you like to understand Chinese history?",
+                "pinyin": "你想了解中国的历史吗?",
+                "pinyin": "Nǐ xiǎng liǎojiě zhōngguó de lìshǐ ma?",
+            },
+            {
+                "english": "They usually go running or hiking on Sundays.",
+                "pinyin": "星期天她们一般去跑步或者爬山.",
+                "pinyin": "Xīngqítiān tāmen yībān qù pǎobù huòzhě páshān.",
+            },
+            {
+                "english": "She needs to buy a new bicycle.",
+                "pinyin": "她要买新的自行车.",
+                "pinyin": "Tā yāomǎi xīn de zìxíngchē.",
+            },
+            {
+                "english": "Where is my bicyle?",
+                "chinese": "我的自行车在哪儿?",
+                "pinyin": "Wǒ de zìxíngchē zài nǎ'er?",
+            },
+            {
+                "english": "In order to learn to cook French cuisine, he went to France.",
+                "chinese": "为了学习做法国菜, 他去了法国.",
+                "pinyin": "Wèile xuéxí zuò fàguó cài, tā qùle fàguó.",
+            },
+        ],
+    },
+    "unit 59": {
+        "description": "Learn usefule phrases.",
+        "guide_book": "https://www.duolingo.com/guidebook/zh/59",
+        "phrases": [
+            {
+                "english": "Duolingo is my favorite app.",
+                "pinyin": "多邻国是我最喜欢的应用.",
+                "pinyin": "Duō lín guó shì wǒ zuì xǐhuān de yìngyòng.",
+            },
+            {
+                "english": "I want to top up 100 Yuan.",
+                "pinyin": "我想充值一百块.",
+                "pinyin": "Wǒ xiǎng chōngzhí yībǎi kuài.",
+            },
+            {
+                "english": "I can speak fluent Spanish.",
+                "pinyin": "我能说流利的西班牙语.",
+                "pinyin": "Wǒ néng shuō liúlì de xībānyá yǔ.",
+            },
+            {
+                "english": "This dictionary app is good.",
+                "pinyin": "这个词典应用很好.",
+                "pinyin": "Zhège cídiǎn yìngyòng hěn hǎo.",
+            },
+            {
+                "english": "Please don't smoke anymore.",
+                "pinyin": "请别再抽烟了.",
+                "pinyin": "Qǐng bié zài chōuyānle.",
+            },
+            {
+                "english": "Do you know any good apps?",
+                "pinyin": "你知道什么很好的应用吗?",
+                "pinyin": "Nǐ zhīdào shénme hěn hǎo de yìngyòng ma.",
+            },
+            {
+                "english": "I want to buy a Chinese dictionary.",
+                "pinyin": "我想买一本中文词典.",
+                "pinyin": "Wǒ xiǎng mǎi yī běn zhōngwén cídiǎn.",
+            },
+            {
+                "english": "I have gained more than ten kilograms.",
+                "pinyin": "我胖了十多公斤.",
+                "pinyin": "Wǒ pàngle shí duō gōngjīn.",
+            },
+            {
+                "english": "You have to go to the domestic airport.",
+                "pinyin": "你们要去国内机场.",
+                "pinyin": "Nǐmen yào qù guónèi jīchǎng.",
+            },
+            {
+                "english": "I need to charge my phone.",
+                "pinyin": "我的电话要充电.",
+                "pinyin": "Wǒ de diànhuà yào chōngdiàn.",
+            },
+            {
+                "english": "My boyfriend is sick, I would like to take a day off to take care of him.",
+                "pinyin": "我的男朋友生病了, 我想请假照顾他.",
+                "pinyin": "Wǒ de nán péngyǒu shēngbìngle, wǒ xiǎng qǐngjià zhàogù tā.",
+            },
+            {
+                "english": "I think Chinese grammars not very difficult.",
+                "pinyin": "我觉得中文的语法不太难.",
+                "pinyin": "Wǒ juédé zhōngwén de yǔfǎ bù tài nán.",
+            },
+            {
+                "english": "I think you lost weight.",
+                "pinyin": "我觉得你瘦了.",
+                "pinyin": "Wǒ juédé nǐ shòule.",
+            },
+            {
+                "english": "We don't need a tour guide.",
+                "pinyin": "我们不需要导游.",
+                "pinyin": "Wǒmen bù xūyào dǎoyóu.",
+            },
+            {
+                "english": "I want to cancel my room booking.",
+                "pinyin": "我要取消订房.",
+                "pinyin": "Wǒ yào qǔxiāo dìngfáng.",
+            },
+            {
+                "english": "Duolingo said my translation was wrong.",
+                "pinyin": "多邻国说我的翻译是错的.",
+                "pinyin": "Duō lín guó shuō wǒ de fānyì shì cuò de.",
+            },
+            {
+                "english": "I have a fever but don't have a cough.",
+                "pinyin": "我发烧, 但是不咳嗽.",
+                "pinyin": "Wǒ fāshāo, dànshì bù késòu.",
+            },
+            {
+                "english": "Which language's grammar  is the most difficult?",
+                "pinyin": "什么语言的语法最难?",
+                "pinyin": "Shénme yǔyán de yǔfǎ zuì nán?",
+            },
+            {
+                "english": "I will go travelling, can you please help to take care of my dog?",
+                "pinyin": "我会去旅游, 你可以帮我照顾我的狗吗?",
+                "pinyin": "Wǒ huì qù lǚyóu, nǐ kěyǐ bāng wǒ zhàogù wǒ de gǒu ma?",
+            },
+            {
+                "english": "Where can we buy a phone card?",
+                "pinyin": "我们可以在哪儿买电话卡?",
+                "pinyin": "Wǒmen kěyǐ zài nǎ'er mǎi diànhuàkǎ?",
+            },
+            {
+                "english": "I downloaded many games.",
+                "pinyin": "我下载了很多游戏.",
+                "pinyin": ".",
+            },
+            {
+                "english": "Did you guys book a room?",
+                "pinyin": "你们有订房吗?",
+                "pinyin": "Nǐmen yǒu dìngfáng ma?",
+            },
+            {
+                "english": "She likes to smoke after drinking alcohol.",
+                "pinyin": "她喜欢抽烟后喝酒.",
+                "pinyin": ".",
+            },
+            {
+                "english": "This is not a simple question.",
+                "pinyin": "这不是一个简单的问题.",
+                "pinyin": "Tā xǐhuān chōuyān hòu hējiǔ.",
+            },
+            {
+                "english": "We will change planes at the Hong Kong airport.",
+                "pinyin": "我们会在香港机场转机.",
+                "pinyin": "Wǒmen huì zài xiānggǎng jīchǎng zhuǎnjī.",
+            },
+            {
+                "english": "What does this sentence mean?",
+                "pinyin": "这个句子是什么意思?",
+                "pinyin": "Zhège jùzi shì shénme yìsi?",
+            },
+            {
+                "english": "I use Duolingo to learn Chinese.",
+                "pinyin": "我用多邻国学习中文.",
+                "pinyin": "Wǒ yòng duōlínguó xuéxí zhōngwén.",
+            },
+            {
+                "english": "Is there a bank around here?",
+                "pinyin": "附近有没有银行?",
+                "pinyin": "Fùjìn yǒu méiyǒu yínháng?",
+            },
+            {
+                "english": "He lost five kilograms.",
+                "pinyin": "他瘦了五公斤",
+                "pinyin": "Tā shòule wǔ gōngjīn.",
+            },
+            {
+                "english": "He has lived in China, he can speak fluent Chinese.",
+                "pinyin": "他在中国住过, 能说流利的中文.",
+                "pinyin": "Tā zài zhōngguó zhùguò, néng shuō liúlì de zhōngwén.",
+            },
+            {
+                "english": "Excuse me, where can we exchange money?",
+                "pinyin": "请问, 我们可以在哪里换钱?",
+                "pinyin": "Qǐngwèn, wǒmen kěyǐ zài nǎlǐ huànqián?",
+            },
+            {
+                "english": "I need to go to the bank to exchange money.",
+                "pinyin": "我要去银行换钱.",
+                "pinyin": "Wǒ yào qù yínháng huànqián.",
+            },
+            {
+                "english": "I can only answer simple questions.",
+                "pinyin": "我只能回答简单的问题.",
+                "pinyin": "Wǒ zhǐ néng huídá jiǎndān de wèntí.",
+            },
+            {
+                "english": "How do you translate this sentence?",
+                "pinyin": "你怎么翻译这个句子?",
+                "pinyin": "Nǐ zěnme fānyì zhège jùzi?",
+            },
+            {
+                "english": "Who can answer this question?",
+                "pinyin": "谁能回答这个问题?",
+                "pinyin": "Shéi néng huídá zhège wèntí?",
+            },
+            {
+                "english": "I don't have international roaming.",
+                "pinyin": "我没有国际漫游.",
+                "pinyin": "Wǒ méiyǒu guójì mànyóu.",
+            },
+            {
+                "english": "I think my translation is right.",
+                "pinyin": "我觉得我的翻译是对的",
+                "pinyin": "Wǒ juédé wǒ de fānyì shì duì de.",
+            },
+            {
+                "english": "You can't smoke here.",
+                "pinyin": "你不可以在这儿抽烟.",
+                "pinyin": "Nǐ bù kěyǐ zài zhè'er chōuyān.",
+            },
+            {
+                "english": "What time do we need to check out?",
+                "pinyin": "我们要几点退房?",
+                "pinyin": "Wǒmen yào jǐ diǎn tuì fáng?",
+            },
+            {
+                "english": "Where is the transfer counter?",
+                "pinyin": "转机柜台在哪里?",
+                "pinyin": "Zhuǎnjī guìtái zài nǎlǐ?",
+            },
+            {
+                "english": "Is this the international airport?",
+                "pinyin": "这里是国际机场吗?",
+                "pinyin": "Zhèlǐ shì guójì jīchǎng ma?",
+            },
+            {
+                "english": "You have to download the Duolingo app.",
+                "pinyin": "你要下载多邻国的应用.",
+                "pinyin": "Nǐ yào xiàzài duō lín guó de yìngyòng.",
+            },
+        ],
+    },
+}
